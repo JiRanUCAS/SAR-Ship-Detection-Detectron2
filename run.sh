e60a89b65114b46f197044fe9bdd995ca85d9cab
EdisonLeeeee <cnljt@outlook.com>
2021-01-25 01:03:58 -0500

    add cascade maskrcnn

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 ############################## HRSID ###################################
-python train_retinanet.py \
-  --config-file yamls/HRSID/Retinanet-R18.yaml  \
-  --num-gpus 2 \
-  --dist-url "tcp://127.0.0.1:50001" 
+# python train_cascade_mask_rcnn.py \
+#   --config-file yamls/HRSID/Cascade-MaskRCNN-R50.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001" 
+  
+# python train_retinanet.py \
+#   --config-file yamls/HRSID/Retinanet-R18.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001" 
   
 # python train_retinanet.py \
 #   --config-file yamls/HRSID/Retinanet-R50.yaml  \
@@ -10,10 +15,10 @@ python train_retinanet.py \
 #   --dist-url "tcp://127.0.0.1:50001"
 
 
-python train_retinanet.py \
-  --config-file yamls/HRSID/Faster-RCNN-R18.yaml  \
-  --num-gpus 2 \
-  --dist-url "tcp://127.0.0.1:50001"
+# python train_retinanet.py \
+#   --config-file yamls/HRSID/Faster-RCNN-R18.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001"
   
 # python train_fasterrcnn.py \
 #   --config-file yamls/HRSID/Faster-RCNN-R50.yaml  \
@@ -21,20 +26,25 @@ python train_retinanet.py \
 #   --dist-url "tcp://127.0.0.1:50001"  
     
 ############################## SSDD ###################################
-python train_retinanet.py \
-  --config-file yamls/SSDD/Retinanet-R18.yaml  \
+python train_cascade_mask_rcnn.py \
+  --config-file yamls/SSDD/Cascade-MaskRCNN-R50.yaml  \
   --num-gpus 2 \
-  --dist-url "tcp://127.0.0.1:50001"
+  --dist-url "tcp://127.0.0.1:50001" 
+  
+# python train_retinanet.py \
+#   --config-file yamls/SSDD/Retinanet-R18.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001"
   
 # python train_retinanet.py \
 #   --config-file yamls/SSDD/Retinanet-R50.yaml  \
 #   --num-gpus 2 \
 #   --dist-url "tcp://127.0.0.1:50001"
 
-python train_fasterrcnn.py \
-  --config-file yamls/SSDD/Faster-RCNN-R18.yaml  \
-  --num-gpus 2 \
-  --dist-url "tcp://127.0.0.1:50001"
+# python train_fasterrcnn.py \
+#   --config-file yamls/SSDD/Faster-RCNN-R18.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001"
   
 # python train_fasterrcnn.py \
 #   --config-file yamls/SSDD/Faster-RCNN-R50.yaml  \
@@ -42,20 +52,20 @@ python train_fasterrcnn.py \
 #   --dist-url "tcp://127.0.0.1:50001"
 
 ############################## Ship_Part1 ###################################
-python train_retinanet.py \
-  --config-file yamls/Ship_Part1/Retinanet-R18.yaml  \
-  --num-gpus 2 \
-  --dist-url "tcp://127.0.0.1:50001"
+# python train_retinanet.py \
+#   --config-file yamls/Ship_Part1/Retinanet-R18.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001"
 
 # python train_retinanet.py \
 #   --config-file yamls/Ship_Part1/Retinanet-R50.yaml  \
 #   --num-gpus 2 \
 #   --dist-url "tcp://127.0.0.1:50001"
   
-python train_fasterrcnn.py \
-  --config-file yamls/Ship_Part1/Faster-RCNN-R18.yaml  \
-  --num-gpus 2 \
-  --dist-url "tcp://127.0.0.1:50001"
+# python train_fasterrcnn.py \
+#   --config-file yamls/Ship_Part1/Faster-RCNN-R18.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001"
 
 # python train_fasterrcnn.py \
 #   --config-file yamls/Ship_Part1/Faster-RCNN-R50.yaml  \
@@ -63,10 +73,10 @@ python train_fasterrcnn.py \
 #   --dist-url "tcp://127.0.0.1:50001"
 
 ############################## Ship ###################################
-python train_retinanet.py \
-  --config-file yamls/Ship/Retinanet-R18.yaml  \
-  --num-gpus 2 \
-  --dist-url "tcp://127.0.0.1:50001"
+# python train_retinanet.py \
+#   --config-file yamls/Ship/Retinanet-R18.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001"
   
 # python train_retinanet.py \
 #   --config-file yamls/Ship/Retinanet-R50.yaml  \
@@ -74,10 +84,10 @@ python train_retinanet.py \
 #   --dist-url "tcp://127.0.0.1:50001"
 
   
-python train_fasterrcnn.py \
-  --config-file yamls/Ship/Faster-RCNN-R18.yaml  \
-  --num-gpus 2 \
-  --dist-url "tcp://127.0.0.1:50001"  
+# python train_fasterrcnn.py \
+#   --config-file yamls/Ship/Faster-RCNN-R18.yaml  \
+#   --num-gpus 2 \
+#   --dist-url "tcp://127.0.0.1:50001"  
 
 # python train_fasterrcnn.py \
 #   --config-file yamls/Ship/Faster-RCNN-R50.yaml  \
---
+++ train_cascade_mask_rcnn.py
@@ -0,0 +1,77 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import os
+import shutil
+# important! register your custom datasets
+from register_dataset import *
+
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.engine import default_argument_parser, default_setup, launch
+from detectron2.evaluation import COCOEvaluator
+from detectron2.evaluation.testing import verify_results
+from detectron2.utils import comm
+
+class Trainer(DefaultTrainer):
+    
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        """
+        Create evaluator(s) for a given dataset.
+        This uses the special metadata "evaluator_type" associated with each builtin dataset.
+        For your own dataset, you can simply create an evaluator manually in your
+        script and do not have to worry about the hacky if-else logic here.
+        """
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, f"inference")
+        return COCOEvaluator(dataset_name, ["bbox"], True, output_folder)
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("Misc/cascade_mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("Misc/cascade_mask_rcnn_R_50_FPN_3x.yaml") 
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+#     if os.path.exists(cfg.OUTPUT_DIR):
+#         shutil.rmtree(cfg.OUTPUT_DIR)
+    default_setup(cfg, args)
+    return cfg
+
+def main(args):
+    cfg = setup(args)
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        return res
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop or subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+
+    return trainer.train()
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
+
